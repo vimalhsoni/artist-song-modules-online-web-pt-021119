ad628f5817785c30c8e35797b18f79da6dc0a44e
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-02 01:57:12 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,11 @@
 module Findable
 
-  def find_by_name
-    def self.find_by_name(name)
+    def find_by_name(name)
       self.all.detect{|a| a.name == name}
     end
 
 
-  def self.find_by_name(name)
+  def find_by_name(name)
     self.all.detect{|a| a.name == name}
   end
 
