fd6715f4cd7fdeb9996084a9f3aa3df7277354df
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-02 01:59:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   attr_reader :songs
 
   extend Memorable
+  extend Findable
 
   @@artists = []
 
@@ -13,9 +14,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
